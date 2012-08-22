aa7becacc4f9fc1e8858d2110574c2ade37a7d73
Jeremiah Bagula <bagu.dev@gmail.com>
2012-08-21 22:02:38 -0700

    Add the new command for opening in a new browser

--- Browser.py
+++ Browser.py
@@ -5,7 +5,7 @@ SETTINGS = "browser_open.sublime-settings"
 
 class OpenInNewBrowserWindowCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    
+
     # The settings for the plugin
     config = sublime.load_settings(SETTINGS).get('domains', {})
         
@@ -26,21 +26,17 @@ class OpenInNewBrowserWindowCommand(sublime_plugin.TextCommand):
 class OpenInNewTabCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     # The settings for the plugin
-    config = sublime.load_settings(SETTINGS)
-    domain_map = config.get('domains', {})
-    
-    # Get the filename
-    webfile = self.view.file_name()[16:len(self.view.file_name())]
+    config = sublime.load_settings(SETTINGS).get('domains', {})
 
     # Save the file
-    window = sublime.active_window()
-    window.run_command('save')
-    print "Saved chages to ", webfile
-    
-    for title, domain in domain_map.items():
-      url = domain + webfile
+    sublime.active_window().run_command('save')
+    print "\n\nSaved chages to the current file"
 
-    if webfile.endswith(('.php','.html','.htm','.jsp','.cfm','.aspx','.asp','.xhtml')):
+    #url = "http://10.10.10.12/mobile/"
+    for title, domain in config.items():
+        url = domain + self.view.file_name()[16:len(self.view.file_name())]
+    
+    if self.view.file_name().endswith(('.php','.html','.htm','.jsp','.cfm','.aspx','.asp','.xhtml')):
         webbrowser.open_new_tab(url)
     else:
         print "\nThis is not a browsable file\n"
